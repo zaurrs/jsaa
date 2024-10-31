1829de1fb870a8f85c251be7d38bae989f4eb6a9
zaur <zaurrs-af106@code.edu.az>
2024-10-31 12:51:03 +0400

    js

---
+++ script.js
@@ -0,0 +1,186 @@
+// let x = Number(prompt("mefi musbet"));
+// if (x > 0) {
+//     console.log("eded musbetdir");
+
+// }
+// else if (x == 0) {
+//     console.log("eded ne musbet nede menfidir");
+
+// }
+// else {
+//     console.log("eded menfidir");
+
+// }
+
+
+
+// let n = Number(prompt("tek cut"));
+// if (n % 2 == 0) {
+//     console.log("eded cutdur");
+
+// } else {
+//     console.log("eded tekdir");
+
+// }
+
+
+
+// let bmi, height, wheight;
+// // bmi=Number(prompt("bmi"))
+// wheight = Number(prompt("wheight"))
+// height = Number(prompt("height"))
+// bmi = wheight / (height * height);
+
+// if (bmi < 18.5) {
+//     console.log("ceki az");
+
+// } else if (18.6 < bmi && bmi < 24.9) {
+//     console.log("normal ceki");
+
+// } else if (24.5 < bmi && bmi < 29.9) {
+//     console.log("kilolu");
+
+// }
+// else {
+//     console.log("piylenme");
+
+// }
+
+
+let number=30;
+let randomeded=Number(prompt("ededi daxil edin"))
+if(number==randomeded){
+    console.log("dogru dediniz");
+    
+}else{
+    console.log("dogru demediniz");
+}
+
+
+
+
+// let a, b, c;
+// a = Number(prompt("a"))
+// b = Number(prompt("b"))
+// c = Number(prompt("c"))
+// if (a == b && b == c && c == a) {
+//     console.log("beraber terefli ucbucaq");
+
+// }
+// else if (a == b || b == c || a == c) {
+//     console.log("berabaer yanlli ucbucaq");
+
+// } else {
+//     console.log("sade ucbucaq");
+
+// }
+
+
+// let number, start, end;
+// if (start < number && number < end) {
+//     console.log("number ortadadir");
+
+// } else {
+//     console.log('eded ortada deyil');
+
+// }
+
+
+// let number1=Number(prompt('eded1'))
+// let number2=Number(prompt('eded2'))
+// let op=String(prompt('operator'))
+// let sum;
+
+// if(op=="+"){
+//     sum=number1+number2
+//     console.log(sum);
+    
+// }else if( op=="-")
+// {
+//     sum=number1-number2
+//     console.log(sum);
+// }
+// else if( op=="*")
+//     {
+//         sum=number1*number2
+//         console.log(sum);
+//     }
+//     else{
+//         sum=number1/number2
+//         console.log(sum);
+//     }
+
+
+// let ay = prompt("ay");
+// switch (ay) {
+//     case "yanvar":
+//         console.log(31);
+//         break;
+//     case "fevral":
+//         console.log(28);
+//         break;
+//     case "mart":
+//         console.log(31);
+//         break;
+//     case "aprel":
+//         console.log(30);
+//         break;
+//     case "may":
+//         console.log(31);
+//         break;
+//     case "iyun":
+//         console.log(30);
+//         break;
+//     case "iyul":
+//         console.log(31);
+//         break;
+//     case "avqust":
+//         console.log(31);
+//         break;
+//     case "sentyabr":
+//         console.log(30);
+//         break;
+//     case "oktyabr":
+//         console.log(31);
+//         break;
+//     case "noyabr":
+//         console.log(30);
+//         break;
+//     case "dekabr":
+//         console.log(31);
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// let a=Number(prompt("ilk eded"))
+// let b=Number(prompt('ikinci eded'))
+// let c=Number(prompt('ucuncu eded'))
+
+// if(a>b && b>c){
+//     console.log(a+c);
+    
+// }
+// else if(a>c && c>b){
+//     console.log(a+b);
+    
+// }
+// else if(b>a && b>c){
+//     console.log(b+c);
+    
+// }
+// else if(b>c && c>a){
+//     console.log(b+a);
+    
+// }
+// else if(c>a && a>b){
+//     console.log(c+b);
+    
+// }
+
+// else{
+//     console.log(c+b);
+    
+// }
