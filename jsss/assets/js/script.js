7fccf4244add1977eacc743a0064196722c2bf7b
zaur <zaurrs-af106@code.edu.az>
2024-11-05 13:14:37 +0400

    task

---
+++ jsss/assets/js/script.js
@@ -0,0 +1,54 @@
+// let str = prompt("Soz")
+// let arr = str.split("")
+
+// let say=0
+//  arr.map((herf) => {
+//     if(herf === "a" || herf === "ı" || herf === "o" || herf === "u" || herf === "e" || herf === "ə" || herf === "i" || herf === "ö" || herf === "ü"){
+//         alert(herf)
+//         say++
+//     }
+// })
+// alert(say)
+
+
+
+// const elements=["hello", "world", " this", "is", "great"]
+// let array=(elements)=>{
+//     return array
+
+// }
+
+// console.log(elements.join(" "));
+
+
+
+
+// let arr=(string)=>{
+//     let as
+//     for(let i = 0;i<string.length; i++){
+//         if(string[i]!==" "){
+//             as += string[i]
+//         }
+//     }
+//     return as
+
+// }
+// const result = arr("8 j 8 mBliB8g imjB8B8 jl B")
+// console.log(result);
+
+
+// function remove(string){
+    
+//     return string
+// }
+// let prom = prompt()
+// const result = remove(prom)
+// console.log(result.slice(1, result.length-1));
+
+
+
+// function hi(str,num){
+//     return str.repeat(num)
+// }
+// const result = hi("hello", 6)
+// console.log(result);
