0e478a859286ab69b01b0d021e1cfedf4506586a
Sakiff05 <sakifcf-ap215@code.edu.az>
2024-12-20 12:26:59 +0400

    delete

--- 42-FormikYup/src/components/LoginShema.js
+++
@@ -1,9 +0,0 @@
-import { object, string, number, date } from 'yup';
-import * as Yup from 'yup';
-export const loginSchema = Yup.object({
-    username2: string().required("Username is required"),
-    password2: string()
-        .min(8, 'Password must be 8 characters long')
-        
-   
-});
